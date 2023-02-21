709935e3895ec29f11808ffb2564a7301559bd82
Larry Arrington <larryarrington@pursuit.org>
2023-02-21 12:42:55 -0500

    Errorshandling

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (cart.length === 0) {
+    throw new Error(`The ${cart} is empty`);
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,8 +39,30 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if (!Array.isArray(products) || products.length === 0) {
+    throw new Error("The products array is empty or not an array");
+  }
+  if (typeof min !== "number" || typeof max !== "number") {
+    throw new Error("The min and max parameters must be numbers");
+  }
+  if (max === 0) {
+    throw new Error("The max parameter cannot be 0");
+  }
+  if (min > max) {
+    throw new Error(
+      "The min parameter cannot be greater than the max parameter"
+    );
+  }
+  if (min < 0 || max < 0) {
+    throw new Error("The min and max parameters cannot be negative");
+  }
   const result = [];
   for (let product of products) {
+    if (!product.hasOwnProperty("priceInCents")) {
+      throw new Error(
+        "The products array contains an item without a priceInCents property"
+      );
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +74,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
 }
 
 module.exports = {
