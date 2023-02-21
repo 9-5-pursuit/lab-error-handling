cfc3e53d6546c0cebf9a1c77905d99dd76053c78
Christine Marchese <chm978@gmail.com>
2023-02-21 14:51:03 -0500

    error

--- index.js
+++ index.js
@@ -20,11 +20,19 @@ const exampleProducts = [
 */
 function getCartTotal(cart) {
   let result = 0;
+  if(cart.length === 0) {
+    throw 'error'
+  }
   for (let product of cart) {
     result += product.priceInCents;
+  
+   
   }
   return result;
 }
+
+  
+
 
 /*
   This function should throw an error if:
@@ -37,7 +45,25 @@ function getCartTotal(cart) {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
-  for (let product of products) {
+
+if(products.length === 0) {
+  throw 'error';
+}
+if(typeof min !== 'number' || typeof max !== 'number') {
+  throw 'error'; 
+} if(max === 0) {
+    throw 'error'  
+}
+if(min > max) {
+  throw 'error';
+} if(min < 0 || max < 0) {
+  throw 'error';
+
+}
+ for (let product of products) {
+    if(!product.priceInCents ) {
+      throw 'error';
+ }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,12 +75,18 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+  try {
   const filteredProducts = filterProductsByPriceRange(products, min, max);
+
   const total = getCartTotal(filteredProducts);
-
+   
   return total;
-}
 
+} catch (error) {
+console.log('an error occured');
+return 0;
+ }
+}
 module.exports = {
   getCartTotal,
   filterProductsByPriceRange,
